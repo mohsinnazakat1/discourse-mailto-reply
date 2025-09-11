355aaa25f5698c0dbf0e4a4a5b430d8fcfa0f46b
mohsinnazakat1 <csworld7177@gmail.com>
2025-09-11 16:01:49 +0500

    updated site settins in the plugin rb

--- plugin.rb
+++ plugin.rb
@@ -6,17 +6,26 @@
 
 PLUGIN_NAME = "discourse-mailto-reply".freeze
 
+# Register settings directly in plugin.rb
+register_asset "stylesheets/mailto-reply.scss"
+
 after_initialize do
   Sentry.init do |config|
-    config.dsn = 'https://e8421ac2f8734f24b68c5bb3e9aa4ace@o4509722905673728.ingest.de.sentry.io/4509722907770960'
+    config.dsn = 'https://e5a1464540463ca9c200fe70d33961f2@o4509722905673728.ingest.de.sentry.io/4510000222896208'
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
     config.send_default_pii = true
   end
-  # Check if settings exist and are enabled
-  if defined?(SiteSetting.mailto_reply_enabled) && SiteSetting.mailto_reply_enabled
+  # Add site settings
+  SiteSetting.add_setting(:mailto_reply_enabled, false, type: :bool, client: true)
+  SiteSetting.add_setting(:mailto_reply_address, "", type: :string, client: true)
+  SiteSetting.add_setting(:mailto_reply_include_quoted_text, true, type: :bool, client: true)
+  SiteSetting.add_setting(:mailto_reply_include_headers, true, type: :bool, client: true)
+
+  # Only proceed if enabled
+  if SiteSetting.mailto_reply_enabled
     
     # Load the lib file first
     require_dependency File.expand_path('../lib/mailto_link_generator.rb', __FILE__)
@@ -33,8 +42,5 @@ after_initialize do
     add_to_serializer(:post, :can_mailto_reply) do
       SiteSetting.mailto_reply_enabled && scope.authenticated?
     end
-
-    # Register asset
-    register_asset "stylesheets/mailto-reply.scss"
   end
 end
